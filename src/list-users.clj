2b035175b6ed88c28e65fe0b93552b55b4d76757
Adam Rackis <adamrackis@gmail.com>
2017-05-20 17:12:51 -0500

    map working

--- src/list-users.clj
+++ src/list-users.clj
@@ -10,7 +10,7 @@
   (let [db (db-core/mongo-connect)]
        (mc/find-maps db "users" {} {:email 1 :activated 1 :isPublic 1 :_id 0})))
 
-(doseq [book (active-users)] (prn book))
+;(doseq [book (active-users)] (prn book))
 
 
 (defn users-book-counts []
@@ -20,7 +20,17 @@
         ])))
 
 
-(doseq [grouping (users-book-counts)] (prn grouping))
+(defn user-book-count-lookup []
+  (let [user-counts (users-book-counts)]
+    (reduce (fn [hash user-obj] (assoc hash (:_id user-obj) (:count user-obj))) {} user-counts)))
 
 
+;(doseq [grouping (users-book-counts)] (prn grouping))
+
+(let [map (user-book-count-lookup)]
+  (print "\n\n")
+  (print "Results:")
+  (print (get map "58aaa7cfe0fbfbef000ab7ec"))
+  (print (get map "589ac6158e4394f60026d452"))
+    )
 

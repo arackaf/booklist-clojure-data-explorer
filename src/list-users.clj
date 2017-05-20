b99d98d88b481778327857497f03bd4d3c367e36
Adam Rackis <adamrackis@gmail.com>
2017-05-20 00:59:14 -0500

    user book counts

--- src/list-users.clj
+++ src/list-users.clj
@@ -1,5 +1,5 @@
 (ns my.service.server
-  (:require [monger.core :as mg] [monger.collection :as mc])
+  (:require [monger.core :as mg] [monger.collection :as mc] [monger.operators :refer :all])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
 
@@ -13,4 +13,20 @@
        coll "users"]
        (mc/find-maps db coll {} {:email 1 :activated 1 :isPublic 1 :_id 0})))
 
-(doseq [book (active-users)] (prn book))
+;(doseq [book (active-users)] (prn book))
+
+
+(defn users-book-counts
+  []
+  (let [uri constants/connection-string
+        {:keys [conn, db]} (mg/connect-via-uri uri)
+        coll "books"]
+        (mc/aggregate db coll [
+          {$group {:_id "userId" :count { $sum 1 }}}
+        ])))
+
+
+(doseq [grouping (users-book-counts)] (prn grouping))
+
+
+

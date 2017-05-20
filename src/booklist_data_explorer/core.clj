76544ab929b8b6118b6191dd5d3fb31be28ff7fd
Adam Rackis <adamrackis@gmail.com>
2017-05-19 23:41:02 -0500

    base Clojure project

---
+++ src/booklist_data_explorer/core.clj
@@ -0,0 +1,6 @@
+(ns booklist-data-explorer.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/booklist_data_explorer/core_test.clj
@@ -0,0 +1,7 @@
+(ns booklist-data-explorer.core-test
+  (:require [clojure.test :refer :all]
+            [booklist-data-explorer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

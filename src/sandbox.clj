a2ffc25ced6c285c1c08070ba0c24911b6205901
Adam Rackis <adamrackis@gmail.com>
2017-05-21 20:02:35 -0500

    playing with threading macros

--- src/sandbox.clj
+++ src/sandbox.clj
@@ -3,4 +3,27 @@
 (let [{:keys [a, b]} (oneTwo)]
   (+ a b))
 
-(print foo)
+; (print foo)
+
+
+(def nums '(1 2 3 4 5))
+
+
+
+(def user {:name "Adam"})
+
+
+(user :name)
+
+(:name user)
+
+(map (fn [num] (* num num)) nums)
+
+(filter even? (map #(* %1 %1) nums))
+
+(let [x (->> nums
+          (filter even?)
+          (map #(* % %)))]
+  x)
+
+(> 2 3)
